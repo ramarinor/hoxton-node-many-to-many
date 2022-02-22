69bdafe9d02e13dc9d682f17bd614336d10a9152
Rinor Rama <rinor.rama95@gmail.com>
2022-02-22 22:29:43 +0100

    can create resources

--- index.ts
+++ index.ts
@@ -39,6 +39,22 @@ JOIN applicants ON applicants.id = interviews.applicantId
 WHERE interviews.interviewerId = ?;
 `);
 
+const createApplicant = db.prepare(`
+INSERT INTO applicants (name, email) VALUES (?,?);
+`);
+
+const createInterviewer = db.prepare(`
+INSERT INTO interviewers (name, email) VALUES (?,?);
+`);
+
+const createInterview = db.prepare(`
+INSERT INTO interviews (applicantId, interviewerId, date, score) VALUES (?,?,?,?);
+`);
+
+const getInterviewById = db.prepare(`
+    SELECT * FROM interviews WHERE id = ?;
+`);
+
 app.get('/applicants', (req, res) => {
   const applicants = getAllApplicants.all();
 
@@ -82,6 +98,78 @@ app.get('/interviewers/:id', (req, res) => {
     res.send(interviewer);
   } else {
     res.status(404).send({ error: 'Interviewer not found!' });
+  }
+});
+
+app.post('/applicants', (req, res) => {
+  const { name, email } = req.body;
+
+  const errors = [];
+  if (typeof name !== 'string') errors.push(`name is missing or not a string`);
+  if (typeof email !== 'string')
+    errors.push(`email is missing or not a string`);
+
+  if (errors.length === 0) {
+    const info = createApplicant.run(name, email);
+    const newApplicant = getApplicantById.get(info.lastInsertRowid);
+    res.send(newApplicant);
+  } else {
+    res.status(400).send({ errors: errors });
+  }
+});
+
+app.post('/interviewers', (req, res) => {
+  const { name, email } = req.body;
+
+  const errors = [];
+  if (typeof name !== 'string') errors.push('name is missing or not a string');
+  if (typeof email !== 'string')
+    errors.push('email is missing or not a string');
+
+  if (errors.length === 0) {
+    const info = createInterviewer.run(name, email);
+    const newInterviewer = getInterviewerById.get(info.lastInsertRowid);
+    res.send(newInterviewer);
+  } else {
+    res.status(400).send({ errors: errors });
+  }
+});
+
+app.post('/interviews', (req, res) => {
+  const { applicantId, interviewerId, date, score } = req.body;
+  const errors = [];
+
+  if (typeof applicantId !== 'number') {
+    errors.push({ error: 'applicantId is missing or not a number!' });
+  }
+  if (typeof interviewerId !== 'number') {
+    errors.push({ error: 'interviewerId is missing or not a number!' });
+  }
+  if (typeof date !== 'string') {
+    errors.push({ error: 'date missing or not a string' });
+  }
+  if (typeof score !== 'number' || score < 1 || score > 10) {
+    errors.push({ error: 'score missing or not a number between 1 and 10!' });
+  }
+
+  if (errors.length === 0) {
+    const applicant = getApplicantById.get(applicantId);
+    const interviewer = getInterviewerById.get(interviewerId);
+
+    if (applicant && interviewer) {
+      const result = createInterview.run(
+        applicantId,
+        interviewerId,
+        date,
+        score
+      );
+      const newInterview = getInterviewById.get(result.lastInsertRowid);
+      res.send(newInterview);
+    } else {
+      res.status(404).send({ error: 'Applicant or Interviewer not found!' });
+    }
+  } else {
+    res.status(400).send(errors);
   }
 });
 
